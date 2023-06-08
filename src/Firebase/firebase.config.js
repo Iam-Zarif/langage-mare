215655e58b4dcb63d9ce8e494254945f4cf51bbf
Zarif <zarifraja19@gmail.com>
2023-06-08 19:29:12 +0600

    firebase recap

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,23 +1,14 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   apiKey:import.meta.env.VITE_apiKey,
-//   authDomain:import.meta.env.VITE_authDomain,
-//   projectId:import.meta.env.VITE_projectId,
-//   storageBucket:import.meta.env.VITE_storageBucket,
-//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
-//   appId:import.meta.env.VITE_appId,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCl3kYZ7VdFcqbqGZ9wcquVfxpMqx_y-7U",
-  authDomain: "mare-auth.firebaseapp.com",
-  projectId: "mare-auth",
-  storageBucket: "mare-auth.appspot.com",
-  messagingSenderId: "563809722035",
-  appId: "1:563809722035:web:deadf67e5055b8424ddac6",
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,23 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
+
+const stripePromise = loadStripe(import.meta.env.VITE_pk_payment);
 
 const Payment = () => {
     return (
-        <div>
-            <SectionTitle  subHeading={"Please provide Information"}
-            heading={"Payment"}></SectionTitle>
-            <h1></h1>
-        </div>
+      <div>
+        <SectionTitle
+          subHeading={"Please provide Information"}
+          heading={"Payment"}
+        ></SectionTitle>
+<Elements stripe={stripePromise}>
+    <CheckOutForm></CheckOutForm>
+</Elements>
+        
+      </div>
     );
 };
 
